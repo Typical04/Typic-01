8cdb44b98d1e557a4271b54a42e2a36dc36479ac
hyunjun2949 <hyunjun2949@naver.com>
2017-04-30 16:46:01 +0900

    Movement
    I made the object "move"

---
+++ Assets/Movement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+    public float SpeedX;
+    public float SpeedY;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            Vector3 newPosition = transform.position;
+
+            newPosition.x -= SpeedX * Time.deltaTime;
+            transform.position = newPosition;
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            Vector3 newPosition = transform.position;
+
+            newPosition.x += SpeedX * Time.deltaTime;
+            transform.position = newPosition;
+        }
+    }
+}
